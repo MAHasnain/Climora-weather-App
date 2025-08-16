d74fb50fcd27c565cd3c902ba259eb50343ec066
Muhammad_Atta_Ul_Hasnain <attaulhasnainmaqsoodi@gmail.com>
2025-08-16 11:12:34 +0500

    Refactor weather data error handling and enhance featured cities interaction

--- JS/weatherData.js
+++ JS/weatherData.js
@@ -31,11 +31,11 @@ function userLocalWeather() {
             document.querySelector(".weatherData_section").classList.add("hidden");
             document.querySelector(".errorMsg").classList.remove("hidden");
             if (error.code == error.PERMISSION_DENIED) {
-                document.querySelector("#locationError").textContent = "Climora can't feel your weather without location access. Please enable it.";
+                document.querySelector("#error").textContent = "Climora can't feel your weather without location access. Please enable it.";
             } else if (error.code == error.PERMISSION_UNAVAILABLE) {
-                document.querySelector("#locationError").textContent = "Oops! Something went wrong while getting your location.";
+                document.querySelector("#error").textContent = "Oops! Something went wrong while getting your location.";
             } else {
-                document.querySelector("#locationError").textContent = "❌ Oops! Something went wrong while getting your location."
+                document.querySelector("#error").textContent = "❌ Oops! Something went wrong while getting your location."
             }
         })
 
@@ -48,28 +48,39 @@ function userLocalWeather() {
 }
 userLocalWeather();
 
+const searchInput = document.querySelector("#search_input");
 // search Input ki value pr weather fetch hoga 
 searchBtn.addEventListener('click', async e => {
     e.preventDefault();
-    const searchInput = document.querySelector("#search_input");
+    try {
+        const CITYNAME_URL = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value.toLowerCase().trim()}&appid=${API_KEY}&units=metric`;
 
-    const CITYNAME_URL = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value.toLowerCase().trim()}&appid=${API_KEY}&units=metric`;
+        let response = await fetch(CITYNAME_URL)
+        const data = await response.json();
+        console.log(data);
 
-    let response = await fetch(CITYNAME_URL)
-    const data = await response.json();
-    console.log(data);
+        document.querySelector(".weatherData_section").classList.remove("hidden");
+        document.querySelector(".errorMsg").classList.add("hidden");
 
-    document.querySelector(".weatherData_section").classList.remove("hidden");
-    document.querySelector(".errorMsg").classList.add("hidden");
-
-    tempHeading.textContent = Math.round(data.main.temp) + '\u00B0C';
-    cityName.textContent = data.name;
-    // weather_icon.src = data.weather[0].icon + ".png";
-    cloudy.textContent = data.clouds.all + "%";
-    humidity.textContent = data.main.humidity + "%";
-    wind.textContent = data.wind.speed + "km/h";
-    searchInput.value = ""
+        tempHeading.textContent = Math.round(data.main.temp) + '\u00B0C';
+        cityName.textContent = data.name;
+        // weather_icon.src = data.weather[0].icon + ".png";
+        cloudy.textContent = data.clouds.all + "%";
+        humidity.textContent = data.main.humidity + "%";
+        wind.textContent = data.wind.speed + "km/h";
+        searchInput.value = "";
+    } catch (err) {
+        console.error(err)
+        // console.error(err.message)
+        if (err.status == "404") {
+            document.querySelector(".weatherData_section").classList.add("hidden");
+            document.querySelector(".errorMsg").classList.remove("hidden");
+            document.getElementById("error").textContent = `City name is Invalid.`;
+            searchInput.value = ""
+        }
+    }
 });
+
 // time and date updating function
 function timeUpdate() {
     let currentTime = moment().format('h:mm:ss a, MMMM Do YYYY');
@@ -79,7 +90,51 @@ function timeUpdate() {
 }
 timeUpdate()
 
-// const cities = document.querySelector(".featured_cities").childNodes[0]
-// console.log(cities);
+const cities = document.querySelector("#featured_cities")
 
-let featured_cities = [];
+// cities.addEventListener("click", e => {
+//     e.preventDefault()
+//     console.log(e.target.value);
+// })
+
+cities.childNodes.forEach(city => {
+    // if (elem.firstChild !== null) {
+    // let data = elem.firstChild.nodeValue
+
+    city.addEventListener("click", async e => {
+        e.preventDefault()
+        try {
+
+            console.log(city);
+            const CITYNAME_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city.textContent.toLowerCase()}&appid=${API_KEY}&units=metric`;
+
+            let response = await fetch(CITYNAME_URL)
+            let data = await response.json();
+            // console.log(data);
+
+            // active city styling change
+            const cityLis = document.querySelectorAll(".featured_cities")
+            cityLis.forEach(c => c.classList.remove("active-city"))
+            city.classList.add("active-city")
+
+            tempHeading.textContent = Math.round(data.main.temp) + '\u00B0C';
+            cityName.textContent = data.name;
+            // weather_icon.src = data.weather[0].icon + ".png";
+            cloudy.textContent = data.clouds.all + "%";
+            humidity.textContent = data.main.humidity + "%";
+            wind.textContent = data.wind.speed + "km/h";
+
+        } catch (error) {
+            console.error(error)
+        }
+    })
+    // }
+});
+
+// let featured_cities = [];
+// for (let i = 0; i < cities.length; i++) {
+//     featured_cities.push(cities[i]);
+
+// }
+
+// console.log(featured_cities);
